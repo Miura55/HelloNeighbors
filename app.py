c0fea4c5e4a995f345fe9d435adacb97718064d5
Miura55 <acordion.piano@gmail.com>
2019-02-17 16:49:02 +0900

    Check simple test

--- app.py
+++ app.py
@@ -6,8 +6,9 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     title = "Hello"
-    return render_template("index.html", title=title)
+    # return render_template("index.html", title=title)
+    return title
 
 if __name__ == "__main__":
     app.debug = True
-    app.run(host="0.0.0.0")
+    app.run()
